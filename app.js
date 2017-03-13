1ffe034fd245ecd6a319f497d1909c2464204214
wiame <kazdari.wiam@gmail.com>
2017-03-13 09:23:03 +0000

    add require handlebars and add an title test

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
+
+// add handlbars
+var handlebars= require('express-handlebars')
+app.engine('.hbs',handlebars({extname: '.hbs'}));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/', index);
 app.get('/',function (req,res) {
-  res.render('index',{list: ['a','b']});
+  res.render('index',{title: "hello from render hbs" ,list: ['a','b']});
 });
 app.use('/users', users);
 
