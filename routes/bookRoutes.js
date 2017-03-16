e96022bd35982e6e51a96ab258841ec5df705ae9
wiame <kazdari.wiam@gmail.com>
2017-03-16 21:40:03 +0000

    add packages for book service and controller

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -3,7 +3,8 @@ var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
 var router = function (nav) {
-  var bookController = require('../controllers/bookController')(null, nav);
+  var bookService = require('../services/goodReadsService')();
+  var bookController = require('../controllers/bookController')(bookService, nav);
   bookRouter.use(bookController.middlware);
   bookRouter.route('/')
     .get(bookController.getIndex);
