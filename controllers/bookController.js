2814f36545f52282d04342cbd35870e8dff6723e
wiame <kazdari.wiam@gmail.com>
2017-03-16 12:00:37 +0000

    book's controller file

---
+++ controllers/bookController.js
@@ -0,0 +1,48 @@
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+
+var bookController = function(bookService, nav){
+  var middlware = function(req, res, next){
+    next();
+  };
+
+  var getIndex = function (req,res) {
+      var url ='mongodb://localhost:27017/formationnode';
+      mongodb.connect(url,function(err, db){
+        var collection= db.collection('books');
+        collection.find({}).toArray(
+          function(err,results){
+            res.render('books',{
+              title:'Books',
+              nav:nav,
+              books:results
+            });
+        });
+      });
+  };
+
+  var getById = function (req,res) {
+      var id = new objectId(req.params.id);
+      var url = 'mongodb://localhost:27017/formationnode';
+      mongodb.connect(url,function(err, db){
+        var collection=db.collection('books');
+        collection.findOne({_id: id},
+        function(err, results){
+            res.render('book',{
+              title:'book',
+              nav:nav,
+              book:results
+            });
+          }
+        );
+      });
+    }
+
+  return {
+    getIndex: getIndex,
+    getById: getById,
+    middlware: middlware
+  }
+}
+
+module.exports = bookController;
